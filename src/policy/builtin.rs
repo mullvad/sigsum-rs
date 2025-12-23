c6e6d9096c8865f6c79d195d797b5799009812f3
Grégoire Détrez <gregoire@mullvad.net>
2025-12-23 08:25:42 +0100

    fixup! Add built-in policies

--- src/policy/builtin.rs
+++ src/policy/builtin.rs
@@ -1,9 +1,9 @@
 use super::Policy;
 
 const SIGSUM_TEST1_2025: &str = include_str!("sigsum-test1-2025.builtin-policy");
-const SIGSUM_TEST2_2025: &str = include_str!("sigsum-test1-2025.builtin-policy");
-const SIGSUM_TEST_2025_3: &str = include_str!("sigsum-test1-2025.builtin-policy");
-const SIGSUM_GENERAL_2025_1: &str = include_str!("sigsum-test1-2025.builtin-policy");
+const SIGSUM_TEST2_2025: &str = include_str!("sigsum-test2-2025.builtin-policy");
+const SIGSUM_TEST_2025_3: &str = include_str!("sigsum-test-2025-3.builtin-policy");
+const SIGSUM_GENERIC_2025_1: &str = include_str!("sigsum-generic-2025-1.builtin-policy");
 
 impl Policy {
     pub fn builtin(name: &str) -> Option<Self> {
@@ -11,7 +11,7 @@ impl Policy {
             "sigsum-test1-2025" => Some(Policy::parse(SIGSUM_TEST1_2025).unwrap()),
             "sigsum-test2-2025" => Some(Policy::parse(SIGSUM_TEST2_2025).unwrap()),
             "sigsum-test-2025-3" => Some(Policy::parse(SIGSUM_TEST_2025_3).unwrap()),
-            "sigsum-general-2025-1" => Some(Policy::parse(SIGSUM_GENERAL_2025_1).unwrap()),
+            "sigsum-generic-2025-1" => Some(Policy::parse(SIGSUM_GENERIC_2025_1).unwrap()),
             _ => None,
         }
     }
@@ -26,6 +26,6 @@ mod tests {
         assert!(Policy::builtin("sigsum-test1-2025").is_some());
         assert!(Policy::builtin("sigsum-test2-2025").is_some());
         assert!(Policy::builtin("sigsum-test-2025-3").is_some());
-        assert!(Policy::builtin("sigsum-general-2025-1").is_some());
+        assert!(Policy::builtin("sigsum-generic-2025-1").is_some());
     }
 }
